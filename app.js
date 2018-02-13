1c0a2403f594cec627237a3107493071d8d3638d
giansalex <giansalex@gmail.com>
2018-02-13 16:24:30 -0500

    add port from env

--- app.js
+++ app.js
@@ -51,4 +51,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+    console.log('Server running at http://127.0.0.1:' + port + '/');
+});
+
 module.exports = app;
